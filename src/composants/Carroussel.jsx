15d9a232463e3f0dd00c453f8374ab06e186649c
PAUNETantoine <paunet.antoine@gmail.com>
2025-03-31 14:50:55 +0200

    Code pour antonin

--- src/composants/Carroussel.jsx
+++ src/composants/Carroussel.jsx
@@ -1,30 +1,46 @@
-import "../styles/composants/Carroussel.css";
-import React from "react";
+import {useEffect, useState} from "react";
+import "../styles/composants/Carroussel.css"
 
+function Carroussel({ tabElementsEvenement }) {
+    const [elements, setElements] = useState(tabElementsEvenement);
 
-function Carroussel({tabElementsEvenement, tabElementsNewsletter})
-{
-    const [tabEvent, setTabEvent] = React.useState(tabElementsEvenement);
-    const [tabNews, setTabNews] = React.useState(tabElementsNewsletter);
+    useEffect(() => {
+        // Pas besoin de modifier directement le DOM, on utilise React pour gérer les classes
+    }, [elements]); // Exécuté à chaque changement de `elements`
 
+    const nextEvent = () => {
+        setElements((prevElements) => {
+            return [prevElements[2], prevElements[0], prevElements[1]];
+        });
+    };
 
-    return(
-        <div>
-            <h2>Evénements></h2>
-            {tabEvent.map((item, index) => (
-                <div key={index}>
-                    <p>{item.name}</p> {/* Affichage du nom de l'objet */}
-                </div>
-            ))}
-            <br></br>
-            <h2>Newsletter</h2>
-            {tabNews.map((item, index) => (
-                <div key={index}>
-                    <p>{item.name}</p> {/* Affichage du nom de l'objet */}
-                </div>
-            ))}
+    const prevEvent = () => {
+        setElements((prevElements) => {
+            return [prevElements[1], prevElements[2], prevElements[0]];
+        });
+    };
+
+    return (
+        <div id="carroussels">
+            <div className="carroussel-container">
+                {elements.map((item, index) => (
+                    <div
+                        key={index}
+                        className={`ElementCarrousselEvent ${index === 1 ? "selectionne" : index === 0 ? "dessus" : "dessous"}`}
+                        style={{ backgroundImage: `url(${item.Image})` }}
+                    >
+                        <p>{item.Titre}</p>
+                    </div>
+                ))}
+            </div>
+
+            {/* Conteneur des boutons déplacé ici */}
+            <div className="boutons-container">
+                <button className="prev" onClick={prevEvent}>&#60;</button>
+                <button className="next" onClick={nextEvent}>&#62;</button>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Carroussel;
--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,13 +1,48 @@
 import "../styles/pages/Accueil.css";
 import Navbar from "../composants/Navbar";
 import Carroussel from "../composants/Carroussel";
-
+import React from "react";
 
 function Accueil()
 {
+    const [tabElementsEvenement, setTabElementsEvenement] = React.useState([
+        {
+            Titre: "LaserGame 25/04",
+            Image : "/ressources/images/laserGame.webp"
+        },
+        {
+            Titre: "Poker 20/04",
+            Image : "/ressources/images/poker.jpg"
+        },
+        {
+            Titre: "Conference 20/03",
+            Image : "/ressources/images/conférence.png"
+        }
+    ])
+
+    const [tabElementsNewsletter, setTabElementsNewsletter] = React.useState([
+        {
+            Titre: "Le Bde : Au coeur de la vie étudiante",
+            text : "Le bde à pour projet de rassembler les étudiants en informatique afin de leur donne l’oportunité de découvrir le reste de la promo tout en faisant des soirées amusantes avec de nombreuses activités."
+        },
+        {
+            Titre: "Poker 20/04",
+            text : ""
+        },
+        {
+            Titre: "Conference 20/03",
+            text : ""
+        }
+    ])
+
+
+
+
+
     return (
         <div>
             <Navbar></Navbar>
+            <Carroussel tabElementsEvenement={tabElementsEvenement} tabElementsNewsletter={tabElementsNewsletter}></Carroussel>
         </div>
     );
 }
