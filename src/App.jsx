6d1f8e08af565948074bb4343d8f333daa2f8fcd
PAUNETantoine <paunet.antoine@gmail.com>
2025-04-01 10:08:13 +0200

    Code debut mardi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Accueil from './pages/Accueil.jsx';
+import Contact from './pages/Contact.jsx';
 
 
 
--- src/composants/Header.jsx
+++ src/composants/Header.jsx
@@ -1,22 +1,25 @@
 import "../styles/composants/Header.css";
 import Bouton from "./Bouton";
+import { Link } from 'react-router-dom'; // Importation de Link de React Router
 
 function Header()
 {
     return (
         <div>
             <nav>
-                <Bouton texte="Événements" className="header-btn" onClick={() => alert("Événements cliqué !")} />
-                <Bouton texte="Boutique" className="header-btn" onClick={() => alert("Boutique cliqué !")} />
+                <Link to="/evenements"><Bouton texte="Événements" className="header-btn"/></Link>
+                <Link to="/boutique"><Bouton texte="Boutique" className="header-btn"/></Link>
 
-                <Bouton
-                    image={"/ressources/images/logo_bde.png"}
-                    className={"header-btn-image"}
-                    onClick={() => alert("Image cliquée !")}
-                    widthBtn={200}/>
+                <Link to="/">
+                    <Bouton
+                        image={"/ressources/images/logo_bde.png"}
+                        className={"header-btn-image"}
+                        onClick={() => alert("Image cliquée !")}
+                        widthBtn={200}/>
+                </Link>
 
-                <Bouton texte="Contact" className="header-btn" onClick={() => alert("Contact cliqué !")} />
-                <Bouton texte="Se connecter" className="header-btn" onClick={() => alert("Se connecter cliqué !")} />
+                <Link to="/Contact"><Bouton texte="Contact" className="header-btn"/></Link>
+                <Link to="/Connexion"><Bouton texte="Se connecter" className="header-btn"/></Link>
             </nav>
         </div>
     );
